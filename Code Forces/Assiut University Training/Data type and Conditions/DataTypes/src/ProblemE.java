08cec82112178f5c7a9e371a9fb8a209877601f2
Mohamed <mohamed.yasser99999@gmail.com>
2023-08-27 20:50:19 +0300

    updating problem E

--- Code Forces/Assiut University Training/Data type and Conditions/DataTypes/src/ProblemE.java
+++ Code Forces/Assiut University Training/Data type and Conditions/DataTypes/src/ProblemE.java
@@ -5,7 +5,6 @@ public class ProblemE {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double r = scanner.nextDouble();
-
         Double area = Math.pow(r, 2) * Math.PI;
         DecimalFormat decimalFormat = new DecimalFormat("#.000000000");
         System.out.println(decimalFormat.format(area));
