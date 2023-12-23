ed83217cedabc1ed625ba82bad9cfd12d7753af9
Mohamed <mohamed.yasser99999@gmail.com>
2023-12-23 13:02:10 +0200

    Solving: fizz_buzz

---
+++ Leet_Code/src/easy/fizz_buzz/Main.java
@@ -0,0 +1,11 @@
+package easy.fizz_buzz;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<String> result = solution.fizzBuzz(15);
+        System.out.println(result);
+    }
+}
