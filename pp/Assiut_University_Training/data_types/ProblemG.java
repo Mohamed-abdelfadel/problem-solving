7e075a530ecb58a9ccac941216cb531411086d88
Mohamed <mohamed.yasser99999@gmail.com>
2023-12-24 15:05:18 +0200

    Refactoring: Repository Structure

--- pp/Assiut_University_Training/data_types/ProblemG.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class ProblemG {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Long number = scanner.nextLong();
-        System.out.println((number*(number+1))/2);
-    }
-}
